dc2b0725075b2c721bcf3df58d95f0dcf5d13c9e
Jan Kundrát <jan.kundrat@cesnet.cz>
2018-03-02 14:13:37 +0100

    CLI's main() scaffolding
    Again, this is mostly a copy-paste from the other project.
    
    Change-Id: Ic72aee3a46644afc91d9f1d7ca8ab22d477608ef

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2018 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+*/
+
+#include <docopt.h>
+#include <string>
+#include "NETCONF_CLI_VERSION.h"
+
+static const char usage[] =
+R"(CLI interface to remote NETCONF hosts
+
+Usage:
+  netconf-cli (-h | --help)
+  netconf-cli --version
+)";
+
+int main(int argc, char* argv[])
+{
+    auto args = docopt::docopt(usage,
+                               {argv + 1, argv + argc},
+                               true,
+                               "netconf-cli " NETCONF_CLI_VERSION,
+                               true);
+    return 0;
+}
