34c790d5c00c84a14d4b8711dbe48156ce4270ff
Jan Kundrát <jan.kundrat@cesnet.cz>
2018-03-02 13:57:47 +0100

    tests: Add one dummy test
    ...so that cmake has something to build.
    
    Change-Id: I0b1747b51a3a000695ccf5769676035c696e24c5

---
+++ tests/dummy.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2018 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+*/
+
+#include "trompeloeil_catch.h"
+
+TEST_CASE("a do-nothing test sample")
+{
+    REQUIRE(1 == 1);
+}
