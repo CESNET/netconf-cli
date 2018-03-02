f0d6c2de0af1d68e21dbd9ad547a6c11dbb6e620
Václav Kubernát <kubervac@fit.cvut.cz>
2018-03-02 17:28:47 +0100

    add class representing the node tree
    add CParser class
    
    This class holds the definitions of all the grammars. It also holds a reference a CTree instance it uses when parsing. This means, that it doesn't have to be declared globally.
    
    Change-Id: I3ca23253b546e26612106067045e1e7d3e2ef919

---
+++ src/CTree.hpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2018 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubervac@fit.cvut.cz>
+ *
+*/
+
+#pragma once
+
+#include <stdexcept>
+#include <unordered_map>
+#include <unordered_set>
+
+class InvalidNodeException : public std::invalid_argument {
+public:
+    using std::invalid_argument::invalid_argument;
+    ~InvalidNodeException() override;
+
+};
+
+/*! \class CTree
+ *     \brief The class representing the tree, that the user traverses.
+ *
+ *         This class holds the current position in the tree and handles changing the position,
+ *         including checking what nodes are available.
+ *         */
+class CTree {
+public:
+    bool checkNode(const std::string& location, const std::string& node) const;
+    void changeNode(const std::string& node);
+    void initDefault();
+    std::string currentNode() const;
+
+private:
+    void addNode(const std::string& location, const std::string& node);
+    const std::unordered_set<std::string>& children(const std::string& node) const;
+
+    std::unordered_map<std::string, std::unordered_set<std::string>> m_nodes;
+    std::string m_curDir;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,46 @@
 /*
  * Copyright (C) 2018 CESNET, https://photonics.cesnet.cz/
  *
- * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ * Written by Václav Kubernát <kubervac@fit.cvut.cz>
  *
 */
 
+#include <boost/spirit/home/x3.hpp>
 #include <docopt.h>
+#include <iostream>
 #include <string>
+#include "CParser.hpp"
+#include "CTree.hpp"
 #include "NETCONF_CLI_VERSION.h"
 
+
 static const char usage[] =
-R"(CLI interface to remote NETCONF hosts
+    R"(CLI interface to remote NETCONF hosts
 
 Usage:
+  netconf-cli
   netconf-cli (-h | --help)
   netconf-cli --version
 )";
+
+namespace x3 = boost::spirit::x3;
+namespace ascii = boost::spirit::x3::ascii;
+
+using Cmd = std::vector<std::string>;
+using x3::alpha;
+using x3::lit;
+using x3::char_;
+using x3::_attr;
+using x3::lexeme;
+using ascii::space;
+
+
+std::string getInput()
+{
+    std::string line;
+    std::getline(std::cin, line);
+    return line;
+}
 
 int main(int argc, char* argv[])
 {
@@ -24,5 +49,6 @@ int main(int argc, char* argv[])
                                true,
                                "netconf-cli " NETCONF_CLI_VERSION,
                                true);
+    std::cout << "Welcome to netconf-cli" << std::endl;
     return 0;
 }
