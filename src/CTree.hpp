64a7e89ef35bb3e23ee5e8830b4355f2f2f733db
Václav Kubernát <kubervac@fit.cvut.cz>
2018-03-05 17:25:32 +0100

    add class representing the node tree
    Change-Id: Ieccb2df7f27d8ba5b87d198579c0ec1175235c29

---
+++ src/CTree.hpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2018 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubervac@fit.cvut.cz>
+ *
+*/
+
+#pragma once
+
+#include <stdexcept>
+#include <unordered_map>
+#include <unordered_set>
+#include <iostream>
+
+/*! \class CTree
+ *     \brief The class representing the tree, that the user traverses.
+ *
+ *         This class holds the current position in the tree and handles changing the position,
+ *         including checking what nodes are available.
+ *         */
+class CTree {
+public:
+    bool checkNode(const std::string& node) const;
+    void changeNode(const std::string& node);
+    void initDefault();
+        std::string currentNode() const;
+
+private:
+    void addNode(const std::string& location, const std::string& node);
+    const std::unordered_set<std::string>& children(const std::string& node) const;
+
+    std::unordered_map<std::string, std::unordered_set<std::string>> m_nodes;
+    std::string m_cur_dir;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2018 CESNET, https://photonics.cesnet.cz/
  *
- * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ * Written by Václav Kubernát <kubervac@fit.cvut.cz>
  *
 */
 
@@ -35,7 +35,7 @@ namespace ascii = boost::spirit::x3::ascii;
 using Cmd = std::vector<std::string>;
 using x3::alpha;
 using x3::lit;
-using x3::string;
+using x3::char_;
 using x3::_attr;
 using x3::lexeme;
 using ascii::space;
@@ -48,31 +48,25 @@ auto const checkNode =
 {
     std::cout << "trying to enter " << _attr(ctx)<< std::endl;
     
+    tree.checkNode(std::string(tree.currentNode() == "" ? "" : "/") + _attr(ctx));
 };
+auto const node_up =
+    x3::string("..");
 auto const nodename =
-    lexeme[(*alpha)] | string("..") ;
+    +alpha;
 auto const cd_args =
-    nodename[checkNode]
+    (
+     (+(node_up >> +(char_('/'))) >> -node_up) |
+     (*(node_up >> +(char_('/'))) >> nodename[checkNode] >> *("/" >> nodename[checkNode]))
+    )
     >> x3::eoi;
 auto const cd = 
-    string("cd")
+    x3::string("cd")
     >> cd_args;
 
 auto const keywords = 
     cd;
 
-void changeDir(const Cmd& args)
-{
-    if (args.at(1) == "") {
-        curDir = "";
-
-    } else {
-        std::string dir = args.at(1);
-        curDir += "/";
-        curDir += dir;
-    }
-    std::cout << "cd to " << curDir << std::endl;
-}
 
 std::string getInput()
 {
@@ -87,15 +81,16 @@ Cmd parseInput(const std::string& line)
     //auto fcmd = [&](auto& val) { cmd.m_keyword += _attr(val); };
     std::cout << "success: "<< x3::phrase_parse(it, line.end(), keywords, space, args) << std::endl;
     std::cout <<"end: "<< std::string(it, line.end())<< std::endl;
+    std::cout << "cd-arg:" <<args.at(1) << std::endl;
     return args;
 }
-/*void handleInput(const TCommand& cmd)
+void handleInput(const Cmd& cmd)
 {
-    if (cmd.m_keyword == "cd")
-        changeDir(cmd.m_args);
+    if (cmd.at(0) == "cd")
+        tree.changeNode(cmd.at(1));
     else
-        std::cout << "I don't know the keyword \"" << cmd.m_keyword << "\"" << std::endl;
-}*/
+        std::cout << "I don't know the keyword \"" << cmd.at(0) << "\"" << std::endl;
+}
 
 
 int main(int argc, char* argv[])
@@ -108,13 +103,11 @@ int main(int argc, char* argv[])
     tree.initDefault();
     std::cout << "Welcome to netconf-cli" << std::endl;
 
-    changeDir(parseInput("cd dnaj"));
-    changeDir(parseInput("cd   dnaj"));
-    changeDir(parseInput("cd   dnaj  "));
-    changeDir(parseInput("cd dnaj "));
-    changeDir(parseInput("   cd dnaj "));
-    changeDir(parseInput("cd "));
-    changeDir(parseInput("cd dnaj "));
+    parseInput("cd aaa");
+    parseInput("cd aaabbb");
+    parseInput("cd ");
+    parseInput("cd bbb");
+    parseInput("cd uuu");
     
 
     return 0;
