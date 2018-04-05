9aa1f05501e3681ac6e10ba07c0618ecb5e40b9c
Václav Kubernát <kubervac@fit.cvut.cz>
2018-04-06 01:35:20 +0200

    add accessing list elements via keys
    Change-Id: I17dfe478a218d4ff53b8033c0bf277f2fafc1aed

--- src/ast.hpp
+++ src/ast.hpp
@@ -11,7 +11,9 @@
 
 #include <boost/fusion/adapted/struct/adapt_struct.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/fusion/include/std_pair.hpp>
 #include <boost/variant.hpp>
+#include <map>
 #include <vector>
 
 #include "CTree.hpp"
@@ -35,8 +37,10 @@ struct ParserContext {
 
 struct parser_context_tag;
 
+using keyValue_ = std::pair<std::string, std::string>;
+
 struct container_ {
-    container_() {}
+    container_() = default;
     container_(const std::string& name);
 
     bool operator==(const container_& b) const;
@@ -46,17 +50,22 @@ struct container_ {
 
 BOOST_FUSION_ADAPT_STRUCT(container_, m_name)
 
+struct list_
+{
+    std::vector<std::string> m_keys;
+};
+
 struct listElement_ {
     listElement_() {}
-    listElement_(const std::string& listName, const std::string& key);
+    listElement_(const std::string& listName, const std::map<std::string, std::string>& keys);
 
     bool operator==(const listElement_& b) const;
 
     std::string m_listName;
-    std::string m_key;
+    std::map<std::string, std::string> m_keys;
 };
 
-BOOST_FUSION_ADAPT_STRUCT(listElement_, m_listName, m_key)
+BOOST_FUSION_ADAPT_STRUCT(listElement_, m_listName, m_keys)
 
 
 struct path_ {
@@ -73,6 +82,7 @@ struct cd_ {
 
 BOOST_FUSION_ADAPT_STRUCT(cd_, m_path)
 
+struct keyValue_class;
 struct identifier_class;
 
 struct listElement_class {
@@ -81,9 +91,14 @@ struct listElement_class {
     {
         auto& parserContext = x3::get<parser_context_tag>(context);
         const auto& tree = parserContext.m_tree;
-        if (tree.isListElement(parserContext.m_curPath, ast.m_listName, ast.m_key)) {
+
+        std::set<std::string> keys;
+        for (const auto& it : ast.m_keys) {
+            keys.insert(it.first);
+        }
+
+        if (tree.isList(parserContext.m_curPath, ast.m_listName, keys)) {
             parserContext.m_curPath += joinPaths(parserContext.m_curPath, ast.m_listName);
-            parserContext.m_curPath += "[" + ast.m_key + "]";
         } else {
             _pass(context) = false;
         }
@@ -120,6 +135,7 @@ struct cd_class {
 };
 
 
+x3::rule<keyValue_class, keyValue_> const keyValue = "keyValue";
 x3::rule<identifier_class, std::string> const identifier = "indentifier";
 x3::rule<listElement_class, listElement_> const listElement = "listElement";
 x3::rule<container_class, container_> const container = "container";
@@ -127,16 +143,17 @@ x3::rule<path_class, path_> const path = "path";
 x3::rule<cd_class, cd_> const cd = "cd";
 
 
+auto const keyValue_def =
+        lexeme[+alnum >> '=' >> +alnum];
+
 auto const identifier_def =
     lexeme[
         ((alpha | char_("_")) >> *(alnum | char_("_") | char_("-") | char_(".")))
     ];
 
-auto const key =
-    identifier;
-
 auto const listElement_def =
-    identifier >> '[' >> key >> ']';
+    identifier >> '[' >> *keyValue >> ']';
+
 auto const container_def =
     identifier;
 
@@ -146,6 +163,7 @@ auto const path_def =
 auto const cd_def =
     lit("cd") >> path >> x3::eoi;
 
+BOOST_SPIRIT_DEFINE(keyValue)
 BOOST_SPIRIT_DEFINE(identifier)
 BOOST_SPIRIT_DEFINE(listElement)
 BOOST_SPIRIT_DEFINE(container)
