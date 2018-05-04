94938b7005404ab83d0638b3baef2463d55c9362
Václav Kubernát <kubervac@fit.cvut.cz>
2018-05-04 15:12:24 +0200

    move joinPaths to utils.cpp
    Change-Id: I8012fa004d174ddf42086b109caab3f2d7857d1e

---
+++ src/utils.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2018 CESNET, https://photonics.cesnet.cz/
+ * Copyright (C) 2018 FIT CVUT, https://fit.cvut.cz/
+ *
+ * Written by Václav Kubernát <kubervac@fit.cvut.cz>
+ *
+*/
+#include "utils.hpp"
+
+std::string joinPaths(const std::string& prefix, const std::string& suffix)
+{
+    if (prefix.empty() || suffix.empty())
+        return prefix + suffix;
+    else
+        return prefix + '/' + suffix;
+}
---
+++ src/utils.hpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2018 CESNET, https://photonics.cesnet.cz/
+ * Copyright (C) 2018 FIT CVUT, https://fit.cvut.cz/
+ *
+ * Written by Václav Kubernát <kubervac@fit.cvut.cz>
+ *
+*/
+#include <string>
+
+std::string joinPaths(const std::string& prefix, const std::string& suffix);
