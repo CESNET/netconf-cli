0445964c2125451e628370a487729f217269a927
Václav Kubernát <kubervac@fit.cvut.cz>
2018-05-25 09:45:32 +0200

    allow moving up a node with cd
    Change-Id: Id1419fddda4aa2d2c16b5775a43c8df58db98c73

--- src/grammars.hpp
+++ src/grammars.hpp
@@ -11,11 +11,13 @@
 #include "ast.hpp"
 #include "ast_handlers.hpp"
 
+
 x3::rule<keyValue_class, keyValue_> const keyValue = "keyValue";
 x3::rule<identifier_class, std::string> const identifier = "identifier";
 x3::rule<listPrefix_class, std::string> const listPrefix = "listPrefix";
 x3::rule<listSuffix_class, std::vector<keyValue_>> const listSuffix = "listSuffix";
 x3::rule<listElement_class, listElement_> const listElement = "listElement";
+x3::rule<nodeup_class, nodeup_> const nodeup = "nodeup";
 x3::rule<container_class, container_> const container = "container";
 x3::rule<path_class, path_> const path = "path";
 x3::rule<cd_class, cd_> const cd = "cd";
@@ -38,11 +40,14 @@ auto const listSuffix_def =
 auto const listElement_def =
         listPrefix > listSuffix;
 
+auto const nodeup_def =
+        lit("..") > x3::attr(nodeup_());
+
 auto const container_def =
         identifier;
 
 auto const path_def =
-        (container | listElement) % '/';
+        (container | listElement | nodeup) % '/';
 
 auto const cd_def =
       lit("cd ") > path >> x3::eoi;
@@ -52,6 +57,7 @@ BOOST_SPIRIT_DEFINE(identifier)
 BOOST_SPIRIT_DEFINE(listPrefix)
 BOOST_SPIRIT_DEFINE(listSuffix)
 BOOST_SPIRIT_DEFINE(listElement)
+BOOST_SPIRIT_DEFINE(nodeup)
 BOOST_SPIRIT_DEFINE(container)
 BOOST_SPIRIT_DEFINE(path)
 BOOST_SPIRIT_DEFINE(cd)
