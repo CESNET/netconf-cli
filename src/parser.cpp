f2e463fea5f7d674d08b8a096c946ffe8ec32ffc
Václav Kubernát <kubervac@fit.cvut.cz>
2018-05-28 15:51:08 +0200

    show keys of lists in the prompt
    Change-Id: I29deb2b3aa80734b675c0c8309f785fa2eed7ccc

--- src/parser.cpp
+++ src/parser.cpp
@@ -5,6 +5,7 @@
  * Written by Václav Kubernát <kubervac@fit.cvut.cz>
  *
 */
+#include <experimental/iterator>
 #include <ostream>
 #include "parser.hpp"
 
@@ -17,6 +18,28 @@ Parser::Parser(const Schema& schema)
     : m_schema(schema)
 {
 }
+
+struct nodeToDataString : public boost::static_visitor<std::string> {
+    std::string operator()(const listElement_& node) const
+    {
+        std::ostringstream res;
+        res << node.m_name + "[";
+        std::transform(node.m_keys.begin(), node.m_keys.end(),
+                       std::experimental::make_ostream_joiner(res, ' '),
+                       [] (const auto& it) { return it.first + "=" + it.second; });
+        res << "]";
+        return res.str();
+    }
+    std::string operator()(const nodeup_&) const
+    {
+        return "..";
+    }
+    template <class T>
+    std::string operator()(const T& node) const
+    {
+        return node.m_name;
+    }
+};
 
 cd_ Parser::parseCommand(const std::string& line, std::ostream& errorStream)
 {
@@ -53,7 +76,7 @@ std::string Parser::currentNode() const
 {
     std::string res;
     for (const auto& it : m_curDir.m_nodes) {
-        res = joinPaths(res, boost::apply_visitor(nodeToString(), it));
+        res = joinPaths(res, boost::apply_visitor(nodeToDataString(), it));
     }
 
     return res;
