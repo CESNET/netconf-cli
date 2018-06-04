b3c5a049a0adf68a1d3c6ddc5f8fdcfd8a20376e
Václav Kubernát <kubervac@fit.cvut.cz>
2018-06-04 18:12:09 +0200

    add leaves
    Change-Id: I962bb4e74f39ec0a838f984b8c13a420ba36d7ce

--- src/ast.cpp
+++ src/ast.cpp
@@ -18,6 +18,16 @@ bool container_::operator==(const container_& b) const
     return this->m_name == b.m_name;
 }
 
+leaf_::leaf_(const std::string& name)
+    : m_name(name)
+{
+}
+
+bool leaf_::operator==(const leaf_& b) const
+{
+    return this->m_name == b.m_name;
+}
+
 listElement_::listElement_(const std::string& listName, const std::map<std::string, std::string>& keys)
     : m_name(listName)
     , m_keys(keys)
@@ -34,6 +44,11 @@ bool path_::operator==(const path_& b) const
     if (this->m_nodes.size() != b.m_nodes.size())
         return false;
     return this->m_nodes == b.m_nodes;
+}
+
+bool set_::operator==(const set_& b) const
+{
+    return this->m_path == b.m_path && this->m_data == b.m_data;
 }
 
 bool cd_::operator==(const cd_& b) const
