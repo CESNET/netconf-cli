610fdc13a6356992568557f57e21ef590edf1945
Václav Kubernát <kubervac@fit.cvut.cz>
2018-07-18 17:18:43 +0200

    Implement YangSchema subclass
    While we do not necessarily want to use `-isystem` for libyang, we
    definitely need to use it for Boost itself. We are only using SPirit X3
    directly, but that one still brings in a great deal of other supporting
    libraries, including variant and a metaprogramming library.
    
    In our CI environment, these are installed into the same prefix. Boost
    itself is OK as we're including it via cmake imported target which uses
    -isystem behind the scene. However, if we then add libyang via a regular
    target_include_directories, the same path gets pushed into the include
    paths, this time without -isystem, but rather as a regular -I. This in
    turn starts affecting Boost, and we're screwed.
    
    Fix this by ensuring that we consider libyang to be a system library,
    too. We won't get annoyed by some extra warnings and therefore we won't
    be able to improve that library as much as the CI would force us
    otherwise :).
    
    Change-Id: I7ec9b28501a46f8b2219f4920cbf5c1e4df59d7e

---
+++ src/yang_schema.cpp
@@ -0,0 +1,226 @@
+/*
+ * Copyright (C) 2018 CESNET, https://photonics.cesnet.cz/
+ * Copyright (C) 2018 FIT CVUT, https://fit.cvut.cz/
+ *
+ * Written by Václav Kubernát <kubervac@fit.cvut.cz>
+ *
+*/
+
+#include <libyang/Libyang.hpp>
+#include <libyang/Tree_Schema.hpp>
+#include "utils.hpp"
+#include "yang_schema.hpp"
+
+
+class YangLoadError : public std::runtime_error {
+public:
+    using std::runtime_error::runtime_error;
+    ~YangLoadError() override = default;
+};
+
+std::string pathToYangAbsSchemPath(const path_& path)
+{
+    std::string res = "/";
+    std::string currentModule;
+
+    for (const auto& it : path.m_nodes) {
+        auto name = nodeToSchemaString(it);
+
+        if (it.m_suffix.type() == typeid(module_)) {
+            currentModule = name;
+            continue;
+        } else {
+            res += currentModule + ":";
+            res += name + "/";
+        }
+    }
+
+    return res;
+}
+
+YangSchema::YangSchema()
+{
+    m_context = std::make_shared<Context>();
+}
+
+void YangSchema::addSchemaString(const char* schema)
+{
+    auto m_moduleTEMP = m_context->parse_module_mem(schema, LYS_IN_YANG);
+    if (!m_moduleTEMP) {
+        throw YangLoadError("Couldn't load schema");
+    }
+}
+
+void YangSchema::addSchemaDirectory(const char* directoryName)
+{
+    if (m_context->set_searchdir(directoryName) == EXIT_FAILURE) {
+        throw YangLoadError("Couldn't add schema search directory");
+    }
+}
+
+void YangSchema::addSchemaFile(const char* filename)
+{
+    auto m_moduleTEMP = m_context->parse_module_path(filename, LYS_IN_YANG);
+    if (!m_moduleTEMP) {
+        throw YangLoadError("Couldn't load schema");
+    }
+}
+
+bool YangSchema::isModule(const path_&, const std::string& name) const
+{
+    auto set = modules();
+    return set.find(name) != set.end();
+}
+
+bool YangSchema::isContainer(const path_& location, const ModuleNodePair& node) const
+{
+    auto set = getNodeSet(location, node);
+    auto schemaSet = std::shared_ptr<std::vector<std::shared_ptr<Schema_Node>>>(set->schema());
+    if (!set)
+        return false;
+    return set->number() == 1 && (*(schemaSet->begin()))->nodetype() == LYS_CONTAINER;
+}
+
+bool YangSchema::isLeaf(const path_& location, const ModuleNodePair& node) const
+{
+    auto set = getNodeSet(location, node);
+    auto schemaSet = std::shared_ptr<std::vector<std::shared_ptr<Schema_Node>>>(set->schema());
+    if (!set)
+        return false;
+    return set->number() == 1 && (*(schemaSet->begin()))->nodetype() == LYS_LEAF;
+}
+
+bool YangSchema::isList(const path_& location, const ModuleNodePair& node) const
+{
+    auto set = getNodeSet(location, node);
+    auto schemaSet = std::shared_ptr<std::vector<std::shared_ptr<Schema_Node>>>(set->schema());
+    if (!set)
+        return false;
+    return set->number() == 1 && (*(schemaSet->begin()))->nodetype() == LYS_LIST;
+}
+
+bool YangSchema::isPresenceContainer(const path_& location, const ModuleNodePair& node) const
+{
+    if (!isContainer(location, node))
+        return false;
+    auto set = getNodeSet(location, node);
+    auto schemaSet = std::shared_ptr<std::vector<std::shared_ptr<Schema_Node>>>(set->schema());
+    auto container = std::make_shared<Schema_Node_Container>(schemaSet->back());
+    return container->presence();
+}
+
+bool YangSchema::leafEnumHasValue(const path_& location, const ModuleNodePair& node, const std::string& value) const
+{
+    if (!isLeaf(location, node) || leafType(location, node) != yang::LeafDataTypes::Enum)
+        return false;
+
+    auto set = getNodeSet(location, node);
+    auto schemaSet = std::shared_ptr<std::vector<std::shared_ptr<Schema_Node>>>(set->schema());
+    auto leaf = std::make_shared<Schema_Node_Leaf>(schemaSet->back());
+    auto enm = std::shared_ptr<std::vector<S_Type_Enum>>(leaf->type()->info()->enums()->enm());
+    for (auto it : *enm) {
+        if (it->name() == value)
+            return true;
+    }
+    return false;
+}
+
+bool YangSchema::listHasKey(const path_& location, const ModuleNodePair& node, const std::string& key) const
+{
+    if (!isList(location, node))
+        return false;
+    auto keys = listKeys(location, node);
+    return keys.find(key) != keys.end();
+}
+
+bool YangSchema::nodeExists(const std::string& location, const std::string& node) const
+{
+    auto absPath = location + "/" + node;
+    auto set = m_context->find_path(absPath.c_str());
+    return set->number() == 1;
+}
+
+S_Set YangSchema::getNodeSet(const path_& location, const ModuleNodePair& node) const
+{
+    std::string absPath = location.m_nodes.empty() ? "" : "/";
+    absPath += pathToAbsoluteSchemaString(location) + "/" + fullNodeName(location, node);
+    auto set = m_context->find_path(absPath.c_str());
+    return set;
+}
+
+const std::set<std::string> YangSchema::listKeys(const path_& location, const ModuleNodePair& node) const
+{
+    std::set<std::string> keys;
+    auto set = getNodeSet(location, node);
+    auto schemaSet = std::shared_ptr<std::vector<std::shared_ptr<Schema_Node>>>(set->schema());
+    auto list = std::make_shared<Schema_Node_List>(schemaSet->back());
+    auto keysVec = std::shared_ptr<std::vector<S_Schema_Node_Leaf>>(list->keys());
+    for (auto it : *keysVec) {
+        keys.insert(it->name());
+    }
+    return keys;
+}
+
+yang::LeafDataTypes YangSchema::leafType(const path_& location, const ModuleNodePair& node) const
+{
+    auto set = getNodeSet(location, node);
+    auto schemaSet = std::shared_ptr<std::vector<std::shared_ptr<Schema_Node>>>(set->schema());
+    auto leaf = std::make_shared<Schema_Node_Leaf>(schemaSet->back());
+
+    switch (leaf->type()->base()) {
+    case LY_TYPE_STRING:
+        return yang::LeafDataTypes::String;
+    case LY_TYPE_DEC64:
+        return yang::LeafDataTypes::Decimal;
+    case LY_TYPE_BOOL:
+        return yang::LeafDataTypes::Bool;
+    case LY_TYPE_INT32:
+        return yang::LeafDataTypes::Int;
+    case LY_TYPE_UINT32:
+        return yang::LeafDataTypes::Uint;
+    case LY_TYPE_ENUM:
+        return yang::LeafDataTypes::Enum;
+    default:
+        return yang::LeafDataTypes::Unknown;
+    }
+}
+
+std::set<std::string> YangSchema::modules() const
+{
+    auto modules = std::shared_ptr<std::vector<S_Module>>(m_context->get_module_iter());
+
+    std::set<std::string> res;
+    std::transform(modules->begin(), modules->end(),
+                   std::inserter(res, res.end()),
+                   [] (auto module) { return module->name(); });
+    return res;
+}
+
+std::set<std::string> YangSchema::childNodes(const path_& path) const
+{
+    std::set<std::string> res;
+    if (path.m_nodes.empty()) {
+        auto lol = std::shared_ptr<std::vector<S_Schema_Node>>(m_context->data_instantiables(0));
+        for (auto it : *lol) {
+            if (std::string(it->module()->name()) == "ietf-yang-library")
+                continue;
+            std::string fullname = it->module()->name();
+            fullname += ":";
+            fullname += it->name();
+            res.insert(fullname);
+        }
+    } else {
+        S_Schema_Node it;
+        auto absolutePath = "/" + pathToAbsoluteSchemaString(path);
+        auto set = m_context->find_path(absolutePath.c_str());
+        auto schemaSet = std::shared_ptr<std::vector<std::shared_ptr<Schema_Node>>>(set->schema());
+        it = (*(schemaSet->begin()))->child();
+        for (; it; it = it->next()) {
+            std::string fullname = it->module()->name();
+            fullname += ":";
+            fullname += it->name();
+            res.insert(fullname);
+        }
+    }
+    return res;
+}
