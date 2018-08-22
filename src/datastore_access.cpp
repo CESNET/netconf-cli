34572f28e5dce569fb3796044fa4abd07072dbe9
Václav Kubernát <kubervac@fit.cvut.cz>
2018-08-22 13:56:35 +0200

    Add abstract class for datastore access
    Change-Id: Ib34ebd24ae639ddd3640e7f6636b236d3232236e

---
+++ src/datastore_access.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2018 CESNET, https://photonics.cesnet.cz/
+ * Copyright (C) 2018 FIT CVUT, https://fit.cvut.cz/
+ *
+ * Written by Václav Kubernát <kubervac@fit.cvut.cz>
+ *
+*/
+
+#include "datastore_access.hpp"
+
+DatastoreAccess::~DatastoreAccess() = default;
