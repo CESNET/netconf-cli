d6e622d28953e886340db4e65800079b1a2fec1e
Václav Kubernát <kubernat@cesnet.cz>
2019-03-07 11:44:48 +0100

    Implement the NetconfAccess class
    Change-Id: Idd63318364edd424e9c477f26db3c6d032721e8a

---
+++ start_daemons.sh
@@ -0,0 +1,9 @@
+set -eux -o pipefail
+shopt -s failglob
+export ASAN_OPTIONS=verify_asan_link_order=false
+~/target/bin/sysrepod
+sleep 1
+~/target/bin/sysrepo-plugind
+sleep 1
+~/target/bin/netopeer2-server -U
+sleep 2
