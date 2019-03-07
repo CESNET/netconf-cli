e1b24b5d4cfc968c5be4eaba280b18095ced4273
Václav Kubernát <kubernat@cesnet.cz>
2019-03-07 11:44:48 +0100

    Implement the NetconfAccess class
    Change-Id: Idd63318364edd424e9c477f26db3c6d032721e8a

--- src/yang_schema.cpp
+++ src/yang_schema.cpp
@@ -7,6 +7,7 @@
 */
 
 #include <libyang/Libyang.hpp>
+#include <libyang/Tree_Data.hpp>
 #include <libyang/Tree_Schema.hpp>
 #include <string_view>
 #include "utils.hpp"
@@ -280,4 +281,17 @@ void YangSchema::registerModuleCallback(const std::function<std::string(const ch
         free(data);
     };
     m_context->add_missing_module_callback(lambda, deleter);
+}
+
+std::shared_ptr<libyang::Data_Node> YangSchema::dataNodeFromPath(const std::string& path, const std::optional<const std::string> value) const
+{
+    if (value)
+        return std::make_shared<libyang::Data_Node>(m_context, path.c_str(), value.value().c_str(), LYD_ANYDATA_CONSTSTRING, 0);
+    else
+        return std::make_shared<libyang::Data_Node>(m_context, path.c_str(), nullptr, LYD_ANYDATA_CONSTSTRING, 0);
+}
+
+std::shared_ptr<libyang::Module> YangSchema::getYangModule(const std::string& name)
+{
+    return m_context->get_module(name.c_str(), nullptr, 0);
 }
