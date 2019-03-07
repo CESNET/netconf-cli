8ef0fd7e61eaaaea18eb3d0f49e78d86b0113b47
Václav Kubernát <kubernat@cesnet.cz>
2019-03-07 11:44:48 +0100

    Implement the NetconfAccess class
    Change-Id: Idd63318364edd424e9c477f26db3c6d032721e8a

--- src/utils.cpp
+++ src/utils.cpp
@@ -91,7 +91,7 @@ struct leafDataToStringVisitor : boost::static_visitor<std::string> {
 
     std::string operator()(const identityRef_& data) const
     {
-        return data.m_value;
+        return data.m_prefix.value().m_name + ":" + data.m_value;
     }
 
     template <typename T>
