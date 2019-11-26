5c83a4348f8f3a3a7db2f93696206a1ace2be132
Václav Kubernát <kubernat@cesnet.cz>
2019-11-26 19:51:31 +0100

    Implement DataQuery
    For now, the DataQuery class includes the listKeys method, which will be
    used in the parser for tab-completion of values of keys.
    
    Change-Id: I2c978762d3a78286b4f7fb97e16118772ddeb7bc

---
+++ src/data_query.cpp
@@ -0,0 +1,49 @@
+#include "data_query.hpp"
+#include "datastore_access.hpp"
+#include "schema.hpp"
+#include "utils.hpp"
+
+
+DataQuery::DataQuery(DatastoreAccess& datastore)
+    : m_datastore(datastore)
+{
+    m_schema = m_datastore.schema();
+}
+
+std::string keyValueToString(const leaf_data_& value)
+{
+    // Only string-like values need to be quoted
+    if (value.type() == typeid(std::string) ||
+            value.type() == typeid(enum_) ||
+            value.type() == typeid(binary_) ||
+            value.type() == typeid(identityRef_) ||
+            value.type() == typeid(bool)) {
+        return escapeListKeyString(leafDataToString(value));
+    }
+    return leafDataToString(value);
+}
+
+std::vector<std::map<std::string, leaf_data_>> DataQuery::listKeys(const dataPath_& location, const ModuleNodePair& node) const
+{
+    std::vector<std::map<std::string, leaf_data_>> res;
+    auto listPath = joinPaths(pathToDataString(location, Prefixes::Always), fullNodeName(location, node));
+
+    auto lists = m_datastore.getItems(listPath);
+
+    decltype(lists) allInstances;
+    std::copy_if(lists.begin(), lists.end(), std::inserter(allInstances, allInstances.end()), [] (const auto& item) {
+        return item.second.type() == typeid(special_) && boost::get<special_>(item.second).m_value == SpecialValue::List;
+    });
+
+    auto keys = m_schema->listKeys(dataPathToSchemaPath(location), node);
+    for (const auto& instance : allInstances) {
+        std::map<std::string, leaf_data_> x;
+        for (const auto& key : keys) {
+            x.insert({key, lists.at(joinPaths(instance.first, key))});
+        }
+        res.push_back(x);
+    }
+
+
+    return res;
+}
