8cd01d7b4880a59f57ac36de2677576d23195068
Václav Kubernát <kubernat@cesnet.cz>
2019-11-26 19:51:31 +0100

    Implement DataQuery
    For now, the DataQuery class includes the listKeys method, which will be
    used in the parser for tab-completion of values of keys.
    
    Change-Id: I2c978762d3a78286b4f7fb97e16118772ddeb7bc

---
+++ src/data_query.hpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2019 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubernat@cesnet.cz>
+ *
+*/
+
+#pragma once
+
+/*! \class DataQuery
+ *  \brief A class used for querying data from a datastore for usage in tab-completion.
+ *
+*/
+
+#include <boost/optional.hpp>
+#include <memory>
+#include <map>
+#include <set>
+
+class DatastoreAccess;
+class Schema;
+struct dataPath_;
+
+using ModuleNodePair = std::pair<boost::optional<std::string>, std::string>;
+class DataQuery {
+public:
+    DataQuery(DatastoreAccess& datastore);
+    /*! \brief Lists all possible key/value pairs for a list specified by the arguments.
+     *  \param location Location of the list.
+     *  \param node Name (and optional module name) of the list.
+     */
+    std::map<std::string, std::set<std::string>> listKeys(const dataPath_& location, const ModuleNodePair& node) const;
+
+private:
+    DatastoreAccess& m_datastore;
+    std::shared_ptr<Schema> m_schema;
+};
