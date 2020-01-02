e5489b3da460f1a81836101eef07416675a85528
Václav Kubernát <kubernat@cesnet.cz>
2020-01-02 19:05:51 +0100

    Integrate DataQuery
    Change-Id: I439374afe485baf08a4d5a1a02fd70d908bec9e1

--- src/ast_handlers.hpp
+++ src/ast_handlers.hpp
@@ -61,7 +61,9 @@ struct node_identifier_class {
     }
 };
 
-struct key_identifier_class;
+struct key_identifier_class {
+
+};
 
 struct module_identifier_class;
 
@@ -587,6 +589,18 @@ struct createKeySuggestions_class {
 
         const auto& keysNeeded = schema.listKeys(parserContext.curPathSchema(), {parserContext.m_curModule, parserContext.m_tmpListName});
         parserContext.m_suggestions = generateMissingKeyCompletionSet(keysNeeded, parserContext.m_tmpListKeys);
+    }
+};
+
+struct createValueSuggestions_class {
+    template <typename T, typename Iterator, typename Context>
+    void on_success(Iterator const& begin, Iterator const&, T&, Context const& context)
+    {
+        auto& parserContext = x3::get<parser_context_tag>(context);
+        const auto& dataQuery = parserContext.m_dataquery;
+
+        parserContext.m_completionIterator = begin;
+        auto keysAndValues = dataQuery.listKeys(parserContext.curPathData(), {parserContext.m_curModule, parserContext.m_tmpListName});
     }
 };
 
