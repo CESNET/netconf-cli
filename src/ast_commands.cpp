9c129116f6d03a83d0eb6aef6445ce6a03149666
Václav Kubernát <kubernat@cesnet.cz>
2020-01-08 14:42:01 +0100

    Remove duplicate definitions of methods
    I have no idea how come this didn't show up as error when I did the
    refactoring, but it did now, so I'm removeing it.
    
    Change-Id: I779d628f3266b75c65d4ceb698dd016bd1f979c5

--- src/ast_commands.cpp
+++ src/ast_commands.cpp
@@ -7,33 +7,6 @@
 */
 #include "ast_commands.hpp"
 
-enum_::enum_() = default;
-
-enum_::enum_(const std::string& value)
-    : m_value(value)
-{
-}
-
-identityRef_::identityRef_() = default;
-
-identityRef_::identityRef_(const std::string& value)
-    : m_value(value)
-{
-}
-
-identityRef_::identityRef_(const std::string& module, const std::string& value)
-    : m_prefix(module_{module})
-    , m_value(value)
-{
-}
-
-binary_::binary_() = default;
-
-binary_::binary_(const std::string& value)
-    : m_value(value)
-{
-}
-
 bool set_::operator==(const set_& b) const
 {
     return this->m_path == b.m_path && this->m_data == b.m_data;
@@ -47,21 +20,6 @@ bool cd_::operator==(const cd_& b) const
 bool ls_::operator==(const ls_& b) const
 {
     return this->m_path == b.m_path && this->m_options == b.m_options;
-}
-
-bool identityRef_::operator==(const identityRef_& b) const
-{
-    return this->m_prefix == b.m_prefix && this->m_value == b.m_value;
-}
-
-bool binary_::operator==(const binary_& b) const
-{
-    return this->m_value == b.m_value;
-}
-
-bool enum_::operator==(const enum_& b) const
-{
-    return this->m_value == b.m_value;
 }
 
 bool create_::operator==(const create_& b) const
