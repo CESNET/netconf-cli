4ee618ab1cdf141a6246bbabf7d6b454c0ec05eb
Václav Kubernát <kubernat@cesnet.cz>
2020-02-05 17:38:13 +0100

    Support connecting to NETCONF clients via SSH
    Change-Id: I27816e038bed0a82a028c8e83c15455fd514c35e

---
+++ src/cli-netconf.hpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2020 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubernat@cesnet.cz>
+ *
+*/
+#include <boost/process.hpp>
+#include <string>
+
+struct OpenSshProcess {
+    boost::process::child process;
+    boost::process::pipe in;
+    boost::process::pipe out;
+};
+OpenSshProcess getSsh(const std::string& hostname);
