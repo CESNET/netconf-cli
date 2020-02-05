9d35c0d2dfe040153d00405548cef6772419129e
Václav Kubernát <kubernat@cesnet.cz>
2020-02-05 17:38:13 +0100

    Support connecting to NETCONF clients via SSH
    libnetconf doesn't have nice APIs for ssh connection, but allows users
    to supply the connection by themselves. One way is to use libssh (which
    libnetconf uses) and supply that. However, I found that using libssh to
    implement an interactive CLI isn't very easy and I'd have to implement a
    lot of functionality (like authentication) by myself, attempts were
    made, but I was really only imitating the interface of OpenSSH.
    Fortunately, libnetconf can also communicate over file descriptors, and
    it is easy to get that from OpenSSH, so I fork it and use its
    stdin/stdout. On top of that, OpenSSH is very clever and knows that I'm
    using it like this, so it still allows entering passwords and accepting
    host keys even though its stdin/stdout isn't a terminal.
    
    Change-Id: I27816e038bed0a82a028c8e83c15455fd514c35e

---
+++ src/cli-netconf.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2020 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubernat@cesnet.cz>
+ *
+*/
+
+#include <boost/spirit/home/x3.hpp>
+#include <boost/fusion/adapted.hpp>
+#include <optional>
+#include <stdexcept>
+#include <unistd.h>
+#include "cli-netconf.hpp"
+
+struct SshOptions {
+    std::string m_hostname;
+    std::optional<unsigned int> m_port = std::nullopt;
+    std::optional<std::string> m_user = std::nullopt;
+};
+
+BOOST_FUSION_ADAPT_STRUCT(SshOptions, m_user, m_hostname, m_port)
+
+namespace {
+namespace cliParsers {
+
+namespace x3 = boost::spirit::x3;
+using x3::alnum;
+using x3::char_;
+using x3::uint_;
+auto atSignIfNotLast = &('@' >> *(char_-'@') >> '@') >> char_("@");
+auto userAllowedChar = x3::rule<class UserAllowedChar, char>{"UserAllowedChar"} = ((char_-'@') | atSignIfNotLast);
+auto user = x3::rule<class User, std::string>{"User"} = +userAllowedChar >> '@';
+auto host = x3::rule<class Host, std::string>{"Host"} = (alnum >> *(-char_('.') >> (alnum | '-')));
+auto port = x3::rule<class Port, unsigned int>{"Port"} = ":" >> uint_;
+
+auto sshOptions = x3::rule<class Host, SshOptions>{"Hostname"} = -user >> host >> -port;
+}
+}
+
+
+SshOptions parseHostname(const std::string& hostname)
+{
+    using namespace std::string_literals;
+    SshOptions res;
+
+    auto it = hostname.begin();
+    auto parseResult = boost::spirit::x3::parse(it, hostname.end(), cliParsers::sshOptions, res);
+    if (!parseResult || it != hostname.end()) {
+        auto error = "Failed to parse hostname here:\n"s;
+        error += hostname + "\n";
+        error += std::string(it - hostname.begin(), ' ');
+        error += '^';
+        throw std::runtime_error{error};
+    }
+
+    return res;
+}
+
+SshProcess sshProcess(const std::string& target)
+{
+    namespace bp = boost::process;
+    auto options = parseHostname(target);
+    bp::pipe in;
+    bp::pipe out;
+    bp::child ssh("ssh " +
+            (options.m_user ? *options.m_user + "@" : "") + options.m_hostname +
+            " -p " + std::to_string(options.m_port.value_or(830)) + " -s netconf",
+            bp::std_out > out, bp::std_in < in);
+
+    return {std::move(ssh), std::move(in), std::move(out)};
+}
