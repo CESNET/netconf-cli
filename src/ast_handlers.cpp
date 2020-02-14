4294a85dca50f1f18e6eb685d2e3539b776cebf3
Václav Kubernát <kubernat@cesnet.cz>
2020-02-14 15:07:14 +0100

    Unify enum and identityref completion
    I could reuse it in the end!
    
    Change-Id: I20abc8eb9305c4420f422c6d99ef3d4552ae336a

--- src/ast_handlers.cpp
+++ src/ast_handlers.cpp
@@ -25,3 +25,15 @@ std::string leafDataToCompletion(const leaf_data_& value)
     }
     return leafDataToString(value);
 }
+
+template<>
+decltype(ParserContext::m_suggestions) createSetSuggestions_class<yang::LeafDataTypes::Enum>::getSuggestions(const ParserContext& parserContext, const Schema& schema) const
+{
+    return schema.enumValues(parserContext.m_tmpListKeyLeafPath.m_location, parserContext.m_tmpListKeyLeafPath.m_node);
+}
+
+template<>
+decltype(ParserContext::m_suggestions) createSetSuggestions_class<yang::LeafDataTypes::IdentityRef>::getSuggestions(const ParserContext& parserContext, const Schema& schema) const
+{
+    return schema.validIdentities(parserContext.m_tmpListKeyLeafPath.m_location, parserContext.m_tmpListKeyLeafPath.m_node, Prefixes::WhenNeeded);
+}
