f0fe769c8d2626586779d12bffb1add9a5f1a135
Václav Kubernát <kubernat@cesnet.cz>
2020-02-19 14:39:47 +0100

    Rename leafrefBase to leafrefBaseType
    leafrefBase could also mean the path of leafref and that's kind of
    misleading.
    
    Change-Id: I56e0b0edd0f21d5ca289ad9cfc4e056d79f6bed0

--- src/ast_handlers.hpp
+++ src/ast_handlers.hpp
@@ -426,7 +426,7 @@ struct leaf_data_base_class {
 
         auto type = schema.leafType(parserContext.m_tmpListKeyLeafPath.m_location, parserContext.m_tmpListKeyLeafPath.m_node);
         if (type == yang::LeafDataTypes::LeafRef) {
-            type = schema.leafrefBase(parserContext.m_tmpListKeyLeafPath.m_location, parserContext.m_tmpListKeyLeafPath.m_node);
+            type = schema.leafrefBaseType(parserContext.m_tmpListKeyLeafPath.m_location, parserContext.m_tmpListKeyLeafPath.m_node);
         }
 
         if (type != TYPE) {
