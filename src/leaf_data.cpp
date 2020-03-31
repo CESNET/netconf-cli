e68201cd28a8646c67516a4be4bbdfa6342b5a5a
Václav Kubernát <kubernat@cesnet.cz>
2020-03-31 02:27:41 +0200

    Rework yang::LeafDataType
    Change-Id: Ie9a30174094b73f2c25af8cfc88d0aa5e9e882b3

--- src/leaf_data.cpp
+++ src/leaf_data.cpp
@@ -7,13 +7,29 @@
 
 #include "leaf_data.hpp"
 template <>
-std::set<std::string> createSetSuggestions_class<yang::LeafDataTypes::Enum>::getSuggestions(const ParserContext& parserContext, const Schema& schema) const
+std::set<std::string> createSetSuggestions_class<yang::Enum>::getSuggestions(ParserContext& parserContext) const
 {
-    return schema.enumValues(parserContext.m_tmpListKeyLeafPath.m_location, parserContext.m_tmpListKeyLeafPath.m_node);
+    auto enums = std::get<yang::Enum>(parserContext.m_leafType).m_allowedValues;
+    std::set<std::string> res;
+    std::transform(enums.begin(), enums.end(), std::inserter(res, res.end()), [] (const auto& value) { return value.m_value; });
+    return res;
 }
 
 template <>
-std::set<std::string> createSetSuggestions_class<yang::LeafDataTypes::IdentityRef>::getSuggestions(const ParserContext& parserContext, const Schema& schema) const
+std::set<std::string> createSetSuggestions_class<yang::IdentityRef>::getSuggestions(ParserContext& parserContext) const
 {
-    return schema.validIdentities(parserContext.m_tmpListKeyLeafPath.m_location, parserContext.m_tmpListKeyLeafPath.m_node, Prefixes::WhenNeeded);
+    auto identities = std::get<yang::IdentityRef>(parserContext.m_leafType).m_allowedValues;
+    std::set<std::string> res;
+    auto topLevelModule = parserContext.currentSchemaPath().m_nodes.front().m_prefix.get().m_name;
+
+    std::transform(identities.begin(), identities.end(), std::inserter(res, res.end()), [topLevelModule] (const auto& identity) {
+        std::string res;
+        if (topLevelModule != identity.m_prefix.get().m_name) {
+            res += identity.m_prefix.get().m_name;
+            res += ":";
+        }
+        res += identity.m_value;
+        return res;
+    });
+    return res;
 }
