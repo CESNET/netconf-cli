3eba10d0100a288542729c32de9dd1def2490f66
Václav Kubernát <kubernat@cesnet.cz>
2020-04-14 01:46:08 +0200

    cli: Add a command line flag to specify datastore
    GCC can't multiline raw string literals in macros (without backslashes which are then included in the string), so I had to
    get rid of the macro.
    
    Change-Id: I1fe2c2f45f2b5700e682c229fdd182edc20e4d77

--- tests/datastore_access.cpp
+++ tests/datastore_access.cpp
@@ -34,7 +34,7 @@ TEST_CASE("setting/getting values")
     SysrepoSubscription subscription("example-schema", &mock);
 
 #ifdef sysrepo_BACKEND
-    SysrepoAccess datastore("netconf-cli-test");
+    SysrepoAccess datastore("netconf-cli-test", Datastore::Running);
 #elif defined(netconf_BACKEND)
     NetconfAccess datastore(NETOPEER_SOCKET_PATH);
 #else
@@ -383,7 +383,7 @@ TEST_CASE("rpc") {
     srSubscription->rpc_subscribe("/example-schema:launch-nukes", cb, nullptr, SR_SUBSCR_CTX_REUSE);
 
 #ifdef sysrepo_BACKEND
-    SysrepoAccess datastore("netconf-cli-test");
+    SysrepoAccess datastore("netconf-cli-test", Datastore::Running);
 #elif defined(netconf_BACKEND)
     NetconfAccess datastore(NETOPEER_SOCKET_PATH);
 #else
