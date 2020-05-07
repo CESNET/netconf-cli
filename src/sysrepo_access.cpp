689854493eff8ead74702667ac7361b67e182a1c
Jan Kundrát <jan.kundrat@cesnet.cz>
2020-05-07 02:15:34 +0200

    Fix `binary` data passing via sysrepo and NETCONF
    Change-Id: I1e30621fa9d1189efd964593e76f381c2cb3378d

--- src/sysrepo_access.cpp
+++ src/sysrepo_access.cpp
@@ -45,6 +45,8 @@ leaf_data_ leafValueFromVal(const sysrepo::S_Val& value)
         auto pair = splitModuleNode(value->data()->get_identityref());
         return identityRef_{*pair.first, pair.second};
     }
+    case SR_BINARY_T:
+        return binary_{value->data()->get_binary()};
     case SR_DECIMAL64_T:
         return value->data()->get_decimal64();
     case SR_CONTAINER_T:
