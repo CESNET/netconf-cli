a93f90041200cd5a120e2e304c79fbf27321889a
Václav Kubernát <kubernat@cesnet.cz>
2020-05-28 03:09:42 +0200

    Refactor ListInstance to a header file
    This typedef is useful for the upcoming `move` command, so I want to
    reuse it. It made no sense to put it in ast_values.hpp, so I just made a
    new header.
    
    Change-Id: Ia1ad07b8ac393a3f1aeffcdd4e19776390a00907

---
+++ src/list_instance.hpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2020 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubernat@cesnet.cz>
+ *
+*/
+#pragma once
+#include <map>
+#include <string>
+#include "ast_values.hpp"
+
+using ListInstance = std::map<std::string, leaf_data_>;
