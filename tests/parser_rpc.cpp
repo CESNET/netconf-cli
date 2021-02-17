aff1f4965d0fc2de3d050d0cd2559d005f3f9d69
Václav Kubernát <kubernat@cesnet.cz>
2021-02-17 10:56:28 +0100

    Fix exec with space and no arguments
    Issue: https://tree.taiga.io/project/jktjkt-netconf-cli/issue/214
    Change-Id: I3efd4fac8c17cbd9006f4f8bbd02a95720d1b89d

--- tests/parser_rpc.cpp
+++ tests/parser_rpc.cpp
@@ -66,5 +66,29 @@ TEST_CASE("rpc")
             input = "exec example:shutdown";
             REQUIRE_THROWS_AS(parser.parseCommand(input, errorStream), InvalidCommandException);
         }
+
+        SECTION("require space between arguments")
+        {
+            input = "execexample:fire";
+            REQUIRE_THROWS_AS(parser.parseCommand(input, errorStream), InvalidCommandException);
+        }
+    }
+
+    SECTION("exec with no arguments")
+    {
+        exec_ execExpected;
+
+        SECTION("without space")
+        {
+            input = "exec";
+        }
+
+        SECTION("with space")
+        {
+            input = "exec ";
+        }
+        command_ command = parser.parseCommand(input, errorStream);
+        auto lol = boost::get<exec_>(command);
+        REQUIRE(boost::get<exec_>(command) == execExpected);
     }
 }
