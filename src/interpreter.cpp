CESNET/netconf-cli

commit 218ee7bcbef1c066a2be56f087ab8c824b529188

Disallow cd out of the prepare context
Issue: https://tree.taiga.io/project/jktjkt-netconf-cli/issue/202
Change-Id: I8e9772ebd1fa86469e2c497b9813686b3ff361d5

src/interpreter.cpp
@@ -89,6 +89,9 @@ void Interpreter::operator()(const get_& get) const
 
 void Interpreter::operator()(const cd_& cd) const
 {
+    if (auto rpcInputPath = m_datastore.inputDatastorePath(); rpcInputPath && !pathToDataString(cd.m_path, Prefixes::WhenNeeded).starts_with(m_parser.currentNode())) {
+        throw std::runtime_error("Can't cd out of `prepare` context");
+    }
     m_parser.changeNode(cd.m_path);
 }
 

tests/interpreter.cpp
@@ -452,6 +452,13 @@ TEST_CASE("rpc")
 
         REQUIRE(parser.currentPath() == rpcPath);
 
+        SECTION("can't leave the context with cd")
+        {
+            REQUIRE_THROWS(boost::apply_visitor(Interpreter(parser, proxyDatastore), command_{cd_{{}, dataPath_{Scope::Absolute, {dataNode_{module_{{"example"}}, container_{"somewhereElse"}}}}}}));
+            REQUIRE_THROWS(boost::apply_visitor(Interpreter(parser, proxyDatastore), command_{cd_{{}, dataPath_{Scope::Relative, {dataNode_{nodeup_{}}}}}}));
+            boost::apply_visitor(Interpreter(parser, proxyDatastore), command_{cancel_{}});
+        }
+
         SECTION("exec")
         {
             REQUIRE_CALL(*input_datastore, getItems("/")).RETURN(DatastoreAccess::Tree{});

commit 865f8fb7892ee1937bd15a8b034182dcd274d33e

Restrict 'cd' to the 'prepare' context
The change follows up the previous fix, which blocked 'cd' command in 'prepare' context entirely.

Bug: https://tree.taiga.io/project/jktjkt-netconf-cli/issue/223
Fixes: 218ee7bcbef1c066a2be56f087ab8c824b529188
Change-Id: Iea0902e7981bcf3f9d41425e58cdd64ef6ea0be0

src/interpreter.cpp
@@ -57,6 +57,16 @@ std::string pathToString(const PathType& path)
     return boost::apply_visitor(pathToStringVisitor(), path);
 }
 
+void Interpreter::checkRpcPath(const dataPath_& commandPath) const
+{
+    if (auto inputPath = m_datastore.inputDatastorePath()) {
+        dataPath_ newPath = realPath(m_parser.currentPath(), commandPath);
+        if (!pathToDataString(newPath, Prefixes::WhenNeeded).starts_with(*inputPath)) {
+            throw std::runtime_error("Can't execute `cd` outside of the `prepare` context.");
+        }
+    }
+}
+
 void Interpreter::operator()(const commit_&) const
 {
     m_datastore.commitChanges();
@@ -98,9 +108,7 @@ void Interpreter::operator()(const get_& get) const
 
 void Interpreter::operator()(const cd_& cd) const
 {
-    if (auto rpcInputPath = m_datastore.inputDatastorePath(); rpcInputPath && !pathToDataString(cd.m_path, Prefixes::WhenNeeded).starts_with(m_parser.currentNode())) {
-        throw std::runtime_error("Can't cd out of `prepare` context");
-    }
+    checkRpcPath(cd.m_path);
     m_parser.changeNode(cd.m_path);
 }
 

src/interpreter.hpp
@@ -36,6 +36,8 @@ struct Interpreter : boost::static_visitor<void> {
     void operator()(const quit_&) const;
 
 private:
+    void checkRpcPath(const dataPath_& commandPath) const;
+
     [[nodiscard]] std::string buildTypeInfo(const std::string& path) const;
 
     template <typename PathType>

tests/interpreter.cpp
@@ -456,6 +456,9 @@ TEST_CASE("rpc")
         {
             REQUIRE_THROWS(boost::apply_visitor(Interpreter(parser, proxyDatastore), command_{cd_{{}, dataPath_{Scope::Absolute, {dataNode_{module_{{"example"}}, container_{"somewhereElse"}}}}}}));
             REQUIRE_THROWS(boost::apply_visitor(Interpreter(parser, proxyDatastore), command_{cd_{{}, dataPath_{Scope::Relative, {dataNode_{nodeup_{}}}}}}));
+
+            // Test that the parser allows to change the current node within the rpc context
+            REQUIRE_NOTHROW(boost::apply_visitor(Interpreter(parser, proxyDatastore), command_{cd_{{}, dataPath_{Scope::Relative, {dataNode_{container_{"payload"}}}}}}));
             boost::apply_visitor(Interpreter(parser, proxyDatastore), command_{cancel_{}});
         }
 