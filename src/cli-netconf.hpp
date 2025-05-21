be182be652298e7f221f91c1b3b0e7b115cbf32b
Tomáš Pecka <tomas.pecka@cesnet.cz>
2025-05-21 11:35:39 +0200

    explicitly use boost::process v1
    Since boost 1.88.0, boost::process v2 was defaulted and v1 deprecated.
    Our code unfortunately uses v1, so the project is unable to build with
    new boost versions [1].
    
    However, the old v1 is still shipped with boost::process, it just needs
    to be included manually and one define that enables the stuff in
    boost::process::v1 namespace to appear in boost::process [2].
    
    [1] https://www.boost.io/doc/libs/1_88_0/doc/html/process.html#version_2
    [2] https://github.com/boostorg/process/issues/480
    
    Change-Id: I72c05271b38c20853b6107f0932062a6458216bc

--- src/cli-netconf.hpp
+++ src/cli-netconf.hpp
@@ -4,7 +4,18 @@
  * Written by Václav Kubernát <kubernat@cesnet.cz>
  *
 */
+
+#include <boost/version.hpp>
+#if BOOST_VERSION < 108800
 #include <boost/process.hpp>
+#else
+#define BOOST_PROCESS_VERSION 1
+#include <boost/process/v1/child.hpp>
+#include <boost/process/v1/io.hpp>
+#include <boost/process/v1/pipe.hpp>
+#include <boost/process/v1/search_path.hpp>
+#endif
+
 #include <string>
 
 struct SshProcess {
